7ba196f2a34808405f693ff50b08a246130a1fc9
Pouya Maroufi <pouyaapps@gmail.com>
2022-01-22 11:15:56 -0800

    theme and color added

---
+++ lib/Theme/color.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+// Main Color
+const mainColor = Color(0xFF300B7F);
+const titleColor = Color(0xFF161C2A);
+const subtitleColor = Color(0xFF647080);
+const textColor = Color(0xFF03C4FE);
+const borderColor = Color(0xFFDDDFE6);
+const backgroundColor = Color(0xFFF2F2F7);
+// Sections Color
---
+++ lib/Theme/theme.dart
@@ -0,0 +1,86 @@
+import 'package:animations/animations.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import './color.dart';
+
+ThemeData theme() {
+  return ThemeData(
+    fontFamily: 'IS',
+    textTheme: textTheme(),
+    appBarTheme: appBarTheme(),
+    inputDecorationTheme: inputDecorationTheme(),
+    scaffoldBackgroundColor: Colors.white,
+    highlightColor: Colors.indigoAccent.withOpacity(0.20),
+    splashColor: Colors.tealAccent.withOpacity(0.15),
+    visualDensity: VisualDensity.adaptivePlatformDensity,
+    pageTransitionsTheme: const PageTransitionsTheme(
+      builders: <TargetPlatform, PageTransitionsBuilder>{
+        TargetPlatform.android: FadeThroughPageTransitionsBuilder(),
+        TargetPlatform.iOS: FadeThroughPageTransitionsBuilder(),
+      },
+    ),
+  );
+}
+
+TextTheme textTheme() {
+  return const TextTheme(
+    bodyText1: TextStyle(color: titleColor),
+    bodyText2: TextStyle(color: subtitleColor),
+    headline1: TextStyle(color: titleColor),
+    headline2: TextStyle(color: titleColor),
+    headline3: TextStyle(color: titleColor),
+    headline4: TextStyle(color: titleColor),
+    headline5: TextStyle(color: titleColor),
+    headline6: TextStyle(color: titleColor),
+  );
+}
+
+AppBarTheme appBarTheme() {
+  return const AppBarTheme(
+    color: Colors.transparent,
+    elevation: 0,
+    systemOverlayStyle: SystemUiOverlayStyle(
+      systemNavigationBarColor: Colors.transparent, // navigation bar color
+      statusBarColor: Colors.transparent, // status bar color
+      statusBarBrightness: Brightness.dark, //status bar brigtness
+      statusBarIconBrightness: Brightness.dark, //status barIcon Brightness
+      systemNavigationBarDividerColor:
+          Colors.transparent, //Navigation bar divider color
+      systemNavigationBarIconBrightness: Brightness.dark,
+    ), //navigation bar icon),
+    iconTheme: IconThemeData(color: mainColor),
+    centerTitle: true,
+    titleTextStyle:
+        TextStyle(color: titleColor, fontFamily: 'IS_M', fontSize: 18),
+  );
+}
+
+OutlineInputBorder outlineBorder() {
+  return OutlineInputBorder(
+    borderRadius: BorderRadius.circular(15),
+    borderSide: const BorderSide(color: borderColor),
+    gapPadding: 15,
+  );
+}
+
+OutlineInputBorder focusBorder() {
+  return OutlineInputBorder(
+    borderRadius: BorderRadius.circular(15),
+    borderSide: const BorderSide(color: mainColor),
+    gapPadding: 10,
+  );
+}
+
+InputDecorationTheme inputDecorationTheme() {
+  return InputDecorationTheme(
+    labelStyle: const TextStyle(color: subtitleColor),
+    hintStyle: TextStyle(color: subtitleColor.withOpacity(0.3)),
+    alignLabelWithHint: true,
+    fillColor: mainColor,
+    //floatingLabelBehavior: FloatingLabelBehavior.always,
+    contentPadding: const EdgeInsets.symmetric(horizontal: 40, vertical: 15),
+    enabledBorder: outlineBorder(),
+    focusedBorder: focusBorder(),
+    border: outlineBorder(),
+  );
+}
